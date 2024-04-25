2ec035127b68102e672822d396713baf620a9318
Samarth <91328372+Sammy1123@users.noreply.github.com>
2024-04-25 14:04:09 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,10 +1,8 @@
 function smallestMissingPositiveInteger(nums) {
   let n = nums.length;
 
-  // First pass: Adjust the array by placing each number in its correct position if possible
   for (let i = 0; i < n; i++) {
       while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
-          // Swap nums[i] with nums[nums[i] - 1]
           let temp = nums[nums[i] - 1];
           nums[nums[i] - 1] = nums[i];
           nums[i] = temp;
