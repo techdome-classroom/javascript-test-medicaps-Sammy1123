db5182f5c4ce29aa385423439e2f68328db91297
Samarth <91328372+Sammy1123@users.noreply.github.com>
2024-04-25 14:06:03 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,15 +1,8 @@
 function longestSubstring(s) {
-  
-    let ans = 0;
-    const ss = new Set();
-    for (let i = 0, j = 0; j < s.length; ++j) {
-        while (ss.has(s[j])) {
-            ss.delete(s[i++]);
-        }
-        ss.add(s[j]);
-        ans = Math.max(ans, j - i + 1);
-    }
-    return ans;
-};
+    // Implementation of longestSubstring function
+    
+}
 
 module.exports = { longestSubstring };
+
+
